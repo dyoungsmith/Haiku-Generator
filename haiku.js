609782f6631408bd0e2dc7607efae7c292382403
Danielle YoungSmith <dyoungsmith@gmail.com>
2016-07-19 17:04:43 -0400

    creating dict

--- haiku.js
+++ haiku.js
@@ -1,10 +1,11 @@
-//Read CMU dict file and convert to human-readable
+//Import FS module
 var fs = require('fs');
-var cmudictFile = readCmudictFile('./cmudict.txt');
 
+//Read CMU dict file and convert to human-readable
 function readCmudictFile(file){
   return fs.readFileSync(file).toString();
-}
+};
+var cmudictFile = readCmudictFile('./cmudict.txt');
 
 
 //Parse the CMU dict file
@@ -15,23 +16,43 @@ function formatData(data){
 		dictBySyl.push([]);	
 	}
 
-	//Loop through each line of CMU dict
-	var lines = data.toString().split("\n"),
-    	lineSplit
+	//Split data file into separate lines (WORD PHONEME\n)
+	var lines = data.toString().split("\n");
+
+	//Create nested array with each line as an array [['WORD', 'PHENOME'], ...]
+	for (var i = 0; i < lines.length; i++){
+		var lineArr = [];
+		lineArr.push(lines[i].split("  "));
+		//console.log(lines[i]);
+	}
+	console.log(lineArr);
+
+	//Populate dictionary arranged as syllable = index
+	for (var i = 0; i < lineArr.length; i++){
+    	var words = lineArr[i][0];	
+    	var phonemes = lineArr[i][1];
+    	// console.log(words);
+    	// console.log(phonemes);	
+    	//var numSyl = phonemes.match(/\d/g).length;
+    	//dictBySyl[numSyl].push(words);
+    }
+	//console.log(dictBySyl);
 
     //Push each word into dictBySyl indexed by number of syllables
     //ex: 'TUNA' is dictBySyl[2][n]
-	lines.forEach(function(line){    
-		var lineSplit = line.split("  "); 
-		//console.log(lineSplit);   
-    	//console.log("The word " + lineSplit[0] + " has this phoneme    layout: " + lineSplit[1]); 
+	// lines.forEach(function(line){    
+	// 	var lineSplit = line.split("  "); 
+
+		// console.log(lineSplit);   
     	//console.log(lineSplit[0] + " $" + lineSplit[1]);
-    	var numSyllables = lineSplit[1].match(/\d/g).length;
-    	//console.log(dictBySyl[lineSplit[1].match(/\d/g).length].push(lineSplit[0]));
-    	console.log(dictBySyl[numSyllables].push(lineSplit[0]));
+    	// var numSyllables = lineSplit[1].match(/\d/g).length;
+    	// console.log(dictBySyl[lineSplit[1].match(/\d/g).length].push(lineSplit[0]));
+    	// console.log(dictBySyl[numSyllables].push(lineSplit[0]));
     	//console.log(dictBySyl[numSyllables]);
-	});   
-}
+	// });
+
+	//console.log(dictBySyl);   
+};
 
 
 //Word and phoneme stash
@@ -57,7 +78,7 @@ function createHaiku(structure){
       return arrOfWords[Math.floor(Math.random() * arrOfWords.length)];
     }).join(' ');
   }).join('\n');
-}*/
+};*/
 
 //Export createHaiku
 module.exports = {
